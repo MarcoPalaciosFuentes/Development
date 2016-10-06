85205d5325bf5c859c7f64aecdc73d1d035f1209
MarcoPalaciosFuentes <marco.palacios.fuentes@gmail.com>
2016-10-06 19:04:29 -0300

    cambios de gemas3

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,3 @@
-# config/initializers/paperclip.rb
+Paperclip::Attachment.default_options[:url] = ':s3-titulo.s3-website-us-west-2.amazonaws.com'
+Paperclip::Attachment.default_options[:path] = ":class/:attachment/:id_partition/:style/:filename"
 Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
